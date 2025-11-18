3670d9f7f7311958822bd8e0a4c2facb5aaa9009
NavilaLabs <steffen.konermann71@gmail.com>
2025-11-18 19:15:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# rust-layers
+A basic template to create new rust projects.
---
+++ crates/application/src/lib.rs
@@ -0,0 +1,19 @@
+mod commands;
+mod queries;
+mod handlers;
+mod services;
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
